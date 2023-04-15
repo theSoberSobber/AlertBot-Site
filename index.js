db74a5b81dc703d36aee2c5aadefa692713542ed
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-04-15 18:15:41 +0530

    basic placeholder for some time

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT || 7000);
-// _____________________________________
-const { getCurrIp } = require('./ipHandler.js');
+app.listen(process.env.PORT || 3001);
+console.log(`Server listening at ${process.env.PORT||3001}`);
 // _____________________________________
 app.get("/", async (req, res) => {
-  res.status(200).send("AlertBot@2022");
+  res.status(200).send(`<b>AlertBot@2023</b> 
+<br/>
+To use AlertBot<sup>TM</sup>, add <a href="tel:+919770483089">it's number</a> to a whatsapp group and type /help.`);
 });
 
-app.get("/:name/group", async (req, res) => {
-    let curr_ip = await getCurrIp();
-    res.redirect(`http://${curr_ip}:3000/${req.params.name}/group/`);
+app.get("/:cName/group", async (req, res) => {
+  res.status(200).send(`We have removed the support for <b>dyanmic groups</b> now. <br />
+Please add <a href="tel:+919770483089">this number</a> to your group and then write "/setup ${req.params.cName}"`);
 });
 
-app.get("/groupIds", async (req, res) => {
-    let curr_ip = await getCurrIp();
-    res.redirect(`http://${curr_ip}:3000/groupIds`);
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.redirect("https://alertbot.vercel.app/");
 });
